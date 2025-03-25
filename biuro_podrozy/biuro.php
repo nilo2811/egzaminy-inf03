40ec81d4a575ef14891b21e7f58ec7a355824c17
Kacper Wójcik <kapik2005@gmail.com>
2025-03-25 22:51:13 +0100

    Add files via upload

---
+++ biuro_podrozy/biuro.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wycieczki krajozawcze</title>
+    <link rel="stylesheet" href="styl.css"/>
+</head>
+<body>
+   
+    <div id="baner">
+        <h1>WITAMY W BIURZE PODRÓŻY</h1>
+    </div>
+    <div id="dane">
+        <h3><span> a</span>rchiwum wycieczek</h3>
+        <table>
+                    <tr>
+                        <td>
+                            <p>5. Francja, Paryz, cena:</p>
+                        </td>
+                        <td>
+                            <p>1350</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>7. Hiszpania, Barcelona, cena:</p>
+                        </td>
+                        <td>
+                        <p>1500</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>8. Hiszpania, Barcelona, cena:</p>
+                        </td>
+                        <td>
+                        <p>1500</p>
+                        </td>
+                    </tr>
+                </table>
+    </div>
+        <section id="lewy">
+            <h3><span> n</span>ajtaniej...
+                <table>
+                    <tr>
+                        <td>
+                            <p>Włochy</p>
+                        </td>
+                        <td>
+                        <p>od 1200 zł</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Francja</p>
+                        </td>
+                        <td>
+                        <p>od 1200 zł</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Hiszpania</p>
+                        </td>
+                        <td>
+                        <p>od 1400 zł</p>
+                        </td>
+                    </tr>
+                </table>
+        </section>
+        <section id="srodkowy">
+            <h3><span> t</span>u byliśmy</h3>
+            <img src="1.jpg">
+            <img src="2.jpg">
+            <img src="3.jpg"><br>
+            <img src="4.jpg">
+            <img src="5.jpg">
+            <img src="6.jpg"><br>
+            <img src="7.jpg">
+            <img src="8.jpg">
+            <img src="9.jpg">
+        </section>
+        <section id="prawy">
+            <h3><span> S</span>kontaktuj się</h3>
+            <a href="wycieczki@wycieczki.pl" onmouseover="najechanie()" onmouseleave="odjazd()">napisz do nas</a>
+            <p>telefon: 555666777</p>
+        </section>
+    <footer><p>Stronę wykonał: <b><i><span>k</span>acper <span>w</span>ójcik 4i</i></b></p></footer>
+
+    <script src="skrypt3.js"></script>
+</body>
+</html>
---
+++ biuro_podrozy/skrypt3.js
@@ -0,0 +1,11 @@
+var link2 = document.querySelector("#prawy a");
+function najechanie(){
+    link2.style.textDecoration = "none";
+    link2.style.FontSize = "xx-large";
+    link2.style.color = "rgb(107, 142, 35)" ;
+}
+function odjazd(){
+    link2.style.textDecoration = "";
+    link2.style.FontSize = "";
+    link2.style.color = "" ;
+}
